1269404c6321ed5cf63361e423dffdad6a8ff01e
Kaie <kkruusma@tktk.ee>
2022-02-19 23:56:07 +0200

    add new projects

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\...\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine(); //või anda ise kausta nimi ette "WishList"
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine(); //või anda ise faili nimi ette "wishlist.txt"
+
+            Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+            File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exists");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
---
+++ ShoppingListToFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingListToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a new item to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your item:");
+                    string userItem = Console.ReadLine();
+                    myShoppingList.Add(userItem);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Have a good day!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string item in myShoppingList)
+            {
+                Console.WriteLine($"Your item:{item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
